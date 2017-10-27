ecd83c5a5604a4639306ac893cb4889dec990153
bscruggs99 <32879353+bscruggs99@users.noreply.github.com>
2017-10-27 08:58:34 -0400

    Add files via upload

---
+++ ArrayPractice.java
@@ -0,0 +1,27 @@
+
+public class ArrayPractice {
+	String name;
+	 int age;
+	public static void main(String[] args) { 
+		// TODO Auto-generated method stub
+		
+		ArrayPractice[] myArray = new ArrayPractice[5];
+
+myArray[1] = new ArrayPractice();
+
+myArray[0] = new ArrayPractice();
+myArray[0].name = "Moo";
+myArray[1].name = "Cow";
+myArray[0].age = 4;
+myArray[1].age = 7;
+if (age < 9 ) {
+	if (name.equalsIgnoreCase("cow")) {
+		System.out.println("hey moo moo");
+	}
+System.out.println(myArray[0].name + myArray[0].age);
+System.out.println(myArray[1].name + myArray[1].age);
+
+}
+	}
+
+}
